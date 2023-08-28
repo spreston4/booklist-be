c633803ad896f8801800962d8d44d4e2bc8e174d
Sam Preston <sam.preston@headway.io>
2023-08-28 11:30:29 -0700

    added registrations_controller & implemented authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,39 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
-    user = User
-            .find_by(email: params["user"]["email"])
-            .try(:authenticate, params["user"]["password"])
-    if user
-      session[:user.id] = user.id
-      render json {
+    user = User.find_by(email: params["user"]["email"])
+
+    if user && user.authenticate(params["user"]["password"])
+      session[:user_id] = user.id
+      render json: {
         status: :created,
         logged_in: true,
         user: user
       }
     else
-      render json { status: 401 }
+      render json: { status: 401 }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   root "static#index"
 end
