5f862ed376120d0ea732f28ad5123b60bdc808a4
Sam Preston <sam.preston@headway.io>
2023-08-31 17:09:12 -0700

    registrations_cotroller refactor: strong params, check for existing user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,32 @@
 class RegistrationsController < ApplicationController
   def create
-    user = User.create!(
-      email: params["user"]["email"], 
-      password: params["user"]["password"],
-      password_confirmation: params["user"]["password_confirmation"]
-      )
+    if User.find_by(email: params["user"]["email"])
+      render json: {
+        status: 409,
+        message: "User already exists"
+      }
+      return
+    end
+    
+    user = User.new(user_params)
 
-    if user
+    if user.save
       session[:user_id] = user.id
       render json: {
         status: :created,
         user: user
       }
     else
-      render json: { status: 500 }
+      render json: { 
+        status: 500,
+        message: "Error creating account" 
+      }
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
